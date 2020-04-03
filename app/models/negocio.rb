1dfa9c90b0a1a348dcc7d741d6f704eddbc2fe7b
Guilherme Froes <guilherme.froes@gmail.com>
2020-04-02 21:46:33 -0300

    Validando tamanho dos campos.

--- app/models/negocio.rb
+++ app/models/negocio.rb
@@ -2,5 +2,18 @@ class Negocio < ApplicationRecord
     belongs_to :user
     belongs_to :categoria
 
-    validates :nome, presence: true
+    validates :nome, presence: true, length: { maximum: 80 }
+    validates :descricao, length: { maximum: 1000 }
+    validates :site, length: { maximum: 80 }
+    validates :cep, length: { maximum: 80 }
+    validates :logradouro, length: { maximum: 80 }
+    validates :numero, length: { maximum: 80 }
+    validates :complemento, length: { maximum: 80 }
+    validates :bairro, length: { maximum: 80 }
+    validates :cidade, length: { maximum: 80 }
+    validates :estado, length: { maximum: 80 }
+    validates :facebook, length: { maximum: 80 }
+    validates :instagram, length: { maximum: 80 }
+    validates :whatsapp, length: { maximum: 80 }
+    validates :telefone, length: { maximum: 80 }
 end
